3278227683364969546f1996148897028827cadc
tyler <tyler@velo.org>
2026-05-01 23:37:54 +0900

    initial commit

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secretKey: 'sdfklsdfslfnlj3j5bj35bj4b4',
+  localDB: 'mongodb://localhost/realestatedb',
+  publicKey: 'aHR0cHM6Ly9hcGkuanNvbnN0b3JhZ2UubmV0L3YxL2pzb24vMmVmOGM3NTgtYTk2Zi00NTllLWIwMzYtYjNiOTAzNzlhMTY1L2ExNzllYTM1LWI5NjItNDcyMi1iM2YxLWUyODMxNmQxYTQ0YQ=='
+}
